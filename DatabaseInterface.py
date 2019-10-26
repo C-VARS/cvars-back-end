44594b3ec9f94c541d3b105be54e4fb91d1f4d6d
Alexander Yang <a.yang7005@gmail.com>
2019-10-26 10:54:43 -0400

    Added a general interface for connection

---
+++ DatabaseInterface.py
@@ -0,0 +1,24 @@
+class DatabaseInterface:
+    """An abstract interface for interaction with a database implementation
+    """
+    def create_invoice(self, json):
+        raise NotImplementedError
+
+    def create_user(self, username: str, user_password: str, user_type: str):
+        raise NotImplementedError
+
+    def attempt_login(self, username: str, user_password: str):
+        raise NotImplementedError
+
+    def get_order_information(self, username: str):
+        raise NotImplementedError
+
+    def assign_driver(self, invoice_id: int, username: str):
+        raise NotImplementedError
+
+    def update_status(self, invoice_id: int, status: str):
+        raise NotImplementedError
+
+    def confirm_payment(self, invoice_id: int):
+        raise NotImplementedError
+
