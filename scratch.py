7ad33af68931f46f7513b06b093cc9bdc377304b
cuppajoeman <callum.cassidynolan@mail.utoronto.ca>
2019-10-26 13:44:41 -0400

    add scratch file

---
+++ scratch.py
@@ -0,0 +1,29 @@
+class DatabaseInterface:
+    """An abstract interface for interaction with a database implementation
+    """
+    def create_invoice(self, json):
+        raise NotImplementedError
+
+    def create_user(self, username: str, user_password: str, user_type: str):
+        raise NotImplementedError
+
+    def attempt_login(self, username: str, user_password: str):
+        # If username & pass are a valid pair
+        # login = (T/F)
+        return (username, pass, type)
+
+    def get_order_information(self, username: str):
+        raise NotImplementedError
+
+    def assign_driver(self, invoice_id: int, username: str):
+        raise NotImplementedError
+
+    def update_status(self, invoice_id: int, status: str):
+        raise NotImplementedError
+
+    def confirm_payment(self, invoice_id: int):
+        raise NotImplementedError
+
+    def initialize(self):
+        raise NotImplementedError
+
