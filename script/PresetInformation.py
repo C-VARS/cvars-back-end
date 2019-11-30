d9418d2160fe32dc155f460e27885816c4f7287d
snam <s.nam@mail.utoronto.ca>
2019-11-30 17:13:30 -0500

    Generated more preset invoices

--- script/PresetInformation.py
+++ script/PresetInformation.py
@@ -2,7 +2,7 @@ Alex = {
     "userType": "Driver",
     "username": "Alex",
     "password": "alex12345",
-    "contact": "2938578",
+    "contact": "416-874-2017",
     "name": "Alexander Yang",
 }
 
@@ -10,7 +10,7 @@ Callum = {
     "userType": "Driver",
     "username": "Callum",
     "password": "callum12345",
-    "contact": "4938279",
+    "contact": "647-298-3784",
     "name": "Callum Cassidy Nolan",
 }
 
@@ -18,7 +18,7 @@ Raag = {
     "userType": "Customer",
     "username": "Raag",
     "password": "raag12345",
-    "contact": "29039459",
+    "contact": "416-284-9927",
     "name": "Raag Kashyap",
     "bankInformation": "ScotiaRaag",
     "address": "123 Business Street"
@@ -28,7 +28,7 @@ Vlad = {
     "userType": "Customer",
     "username": "Vlad",
     "password": "vlad12345",
-    "contact": "2340499",
+    "contact": "647-234-0499",
     "name": "Vladimir Dyagilev",
     "bankInformation": "ScotiaVlad",
     "address": "200 Business Street"
@@ -38,7 +38,7 @@ Sophie = {
     "userType": "Supplier",
     "username": "Sophie",
     "password": "sophie12345",
-    "contact": "13469868",
+    "contact": "905-698-9068",
     "name": "Sophie's Cola",
     "bankInformation": "ScotiaSophieCola",
     "address": "990 Business Street"
@@ -49,12 +49,12 @@ invoice_one = {
     "supplierUsername": "Sophie",
     "driverUsername": "Alex",
     "orders": [{
-        "item": "Normal Coke",
+        "item": "Coca-Cola",
         "price": 2.5,
         "amount": 3
     },
         {
-            "item": "Huge Coke",
+            "item": "Diet Coke",
             "price": 200,
             "amount": 3
         }
@@ -66,14 +66,291 @@ invoice_two = {
     "supplierUsername": "Sophie",
     "driverUsername": "Callum",
     "orders": [{
-        "item": "Cherry Coke",
+        "item": "Cherry Fanta",
         "price": 5.5,
         "amount": 10
     },
         {
-            "item": "Illegal Coke",
+            "item": "Coca-Cola Zero",
             "price": 20000,
             "amount": 1
+        }
+    ]
+}
+
+invoice_three = {
+    "customerUsername": "Raag",
+    "supplierUsername": "Sophie",
+    "driverUsername": "Alex",
+    "orders": [{
+        "item": "Original Sprite",
+        "price": 2.5,
+        "amount": 3
+    },
+        {
+            "item": "Sprite Zero",
+            "price": 2.0,
+            "amount": 3
+        },
+
+        {
+            "item": "Cranberry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Cherry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Lemon Lime Sprite",
+            "price": 2.3,
+            "amount": 4
+        }
+    ]
+}
+
+invoice_four = {
+    "customerUsername": "Raag",
+    "supplierUsername": "Sophie",
+    "driverUsername": "Alex",
+    "orders": [{
+        "item": "Blue Raspberry Powerade",
+        "price": 2.5,
+        "amount": 3
+    },
+        {
+            "item": "Dasani Spring Water",
+            "price": 200,
+            "amount": 3
+        },
+
+        {
+            "item": "Focus Vitaminwater",
+            "price": 3.5,
+            "amount": 10
+        }
+    ]
+}
+
+invoice_five = {
+    "customerUsername": "Raag",
+    "supplierUsername": "Sophie",
+    "driverUsername": "Alex",
+    "orders": [{
+        "item": "Original Sprite",
+        "price": 2.5,
+        "amount": 3
+    },
+        {
+            "item": "Sprite Zero",
+            "price": 2.0,
+            "amount": 3
+        },
+
+        {
+            "item": "Cranberry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Cherry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Lemon Lime Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+{
+        "item": "Original Sprite",
+        "price": 2.5,
+        "amount": 3
+    },
+        {
+            "item": "Sprite Zero",
+            "price": 2.0,
+            "amount": 3
+        },
+
+        {
+            "item": "Cranberry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Cherry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Lemon Lime Sprite",
+            "price": 2.3,
+            "amount": 4
+        }
+    ]
+}
+
+invoice_six = {
+    "customerUsername": "Raag",
+    "supplierUsername": "Sophie",
+    "driverUsername": "Alex",
+    "orders": [{
+        "item": "Original Sprite",
+        "price": 2.5,
+        "amount": 3
+    },
+        {
+            "item": "Sprite Zero",
+            "price": 2.0,
+            "amount": 3
+        },
+
+        {
+            "item": "Cranberry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Cherry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Lemon Lime Sprite",
+            "price": 2.3,
+            "amount": 4
+        }
+    ]
+}
+
+invoice_seven = {
+    "customerUsername": "Raag",
+    "supplierUsername": "Sophie",
+    "driverUsername": "Alex",
+    "orders": [{
+        "item": "Original Sprite",
+        "price": 2.5,
+        "amount": 3
+    },
+        {
+            "item": "Sprite Zero",
+            "price": 2.0,
+            "amount": 3
+        },
+
+        {
+            "item": "Cranberry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Cherry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Lemon Lime Sprite",
+            "price": 2.3,
+            "amount": 4
+        }
+    ]
+}
+invoice_eight = {
+    "customerUsername": "Raag",
+    "supplierUsername": "Sophie",
+    "driverUsername": "Alex",
+    "orders": [{
+        "item": "Original Sprite",
+        "price": 2.5,
+        "amount": 3
+    },
+        {
+            "item": "Sprite Zero",
+            "price": 2.0,
+            "amount": 3
+        },
+
+        {
+            "item": "Cranberry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Cherry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Lemon Lime Sprite",
+            "price": 2.3,
+            "amount": 4
+        }
+    ]
+}
+invoice_nine = {
+    "customerUsername": "Raag",
+    "supplierUsername": "Sophie",
+    "driverUsername": "Alex",
+    "orders": [{
+        "item": "Original Sprite",
+        "price": 2.5,
+        "amount": 3
+    },
+        {
+            "item": "Sprite Zero",
+            "price": 2.0,
+            "amount": 3
+        },
+
+        {
+            "item": "Cranberry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Cherry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Lemon Lime Sprite",
+            "price": 2.3,
+            "amount": 4
+        }
+    ]
+}
+invoice_ten = {
+    "customerUsername": "Raag",
+    "supplierUsername": "Sophie",
+    "driverUsername": "Alex",
+    "orders": [{
+        "item": "Original Sprite",
+        "price": 2.5,
+        "amount": 3
+    },
+        {
+            "item": "Sprite Zero",
+            "price": 2.0,
+            "amount": 3
+        },
+
+        {
+            "item": "Cranberry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Cherry Sprite",
+            "price": 2.3,
+            "amount": 4
+        },
+        {
+            "item": "Lemon Lime Sprite",
+            "price": 2.3,
+            "amount": 4
         }
     ]
 }
