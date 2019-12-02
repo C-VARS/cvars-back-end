d4af1cb5c10835406a77e546c5cebe56e84673ea
Alexander Yang <a.yang7005@gmail.com>
2019-12-02 16:01:05 -0500

    Hid Twilio information as system environment variables

--- script/TwilioMessaging.py
+++ script/TwilioMessaging.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 import os
 
 account_sid = os.environ.get("account_sid",
-                             'AC4164a655996eb4aafa3ba61d40120731')
-auth_token = os.environ.get("auth_token", '1b7955c78b1356c84cf188b8f4a3c758')
+                             'None')
+auth_token = os.environ.get("auth_token", 'None')
 client = Client(account_sid, auth_token)
 
 
